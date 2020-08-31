d8966539eb0bdb5b4a186838b94bd9dcb88537f0
BillyBacker <38096723+BillyBacker@users.noreply.github.com>
2020-08-31 14:53:07 +0700

    - Add fltTuple and fltDic for store float data. - Add Object class for store object in the game. It contained      Object Position, Type of object that can be manually assign by using Is(type) statement and get Object type by using nowIs(), Stat for store unique data for each object that use fltDic so Stat can be add manually for each Object using addStat(Key, value) and using getStat(Key) for get data from Stat.

--- First_step/First_step.cpp
+++ First_step/First_step.cpp
@@ -13,10 +13,13 @@ using namespace sf;
 using namespace std;
 
 class strTuple { // string tuple for store array of string.
-	public:
+public:
 		vector<string> Tuple;
 		string var(int i){
 			return this->Tuple[i];
+		}
+		void set(unsigned __int64 Index, string In) {
+			this->Tuple.at(Index) = In;
 		}
 		void append(string In) {
 			this->Tuple.push_back(In);
@@ -31,7 +34,6 @@ class strTuple { // string tuple for store array of string.
 			return this->Tuple.size();
 		}
 		unsigned __int64 index(string In) {
-			unsigned __int64 num = 0;
 			auto pos = find(this->Tuple.begin(), this->Tuple.end(), In);
 			if (pos != this->Tuple.end()) {
 				unsigned __int64 index = distance(this->Tuple.begin(), pos);
@@ -43,12 +45,47 @@ class strTuple { // string tuple for store array of string.
 		}
 };
 
+class fltTuple { // string tuple for store array of string.
+public:
+	vector<float> Tuple;
+	float var(int i) {
+		return this->Tuple[i];
+	}
+	void set(unsigned __int64 Index, float In) {
+		this->Tuple.at(Index) = In;
+	}
+	void append(float In) {
+		this->Tuple.push_back(In);
+	}
+	void remove(float In) {
+		this->Tuple.erase(std::remove(this->Tuple.begin(), this->Tuple.end(), In), this->Tuple.end());
+	}
+	void popOut(int index) {
+		this->Tuple.erase(Tuple.begin() + index);
+	}
+	unsigned __int64 length() {
+		return this->Tuple.size();
+	}
+	unsigned __int64 index(float In) {
+		auto pos = find(this->Tuple.begin(), this->Tuple.end(), In);
+		if (pos != this->Tuple.end()) {
+			unsigned __int64 index = distance(this->Tuple.begin(), pos);
+			return index;
+		}
+		else {
+			return -1;
+		}
+	}
+};
 class strDic { // string tuple for store array of string.
 public:
 	strTuple Key;
 	strTuple Tuple;
 	string get(string keyIn) {
 		return this->Tuple.var(Key.index(keyIn));
+	}
+	void set(string keyIn, string In) {
+		this->Tuple.set(Key.index(keyIn), In);
 	}
 	void append(string keyIn, string In) {
 		this->Key.append(keyIn);
@@ -63,13 +100,64 @@ public:
 	}
 };
 
+class fltDic { // string tuple for store array of string.
+	public:
+		strTuple Key;
+		fltTuple Tuple;
+		float get(string keyIn) {
+			return this->Tuple.var(Key.index(keyIn));
+		}
+		void set(string keyIn, float In) {
+			this->Tuple.set(Key.index(keyIn), In);
+		}
+		void append(string keyIn, float In) {
+			this->Key.append(keyIn);
+			this->Tuple.append(In);
+		}
+		void remove(string keyIn) {
+			this->Tuple.popOut(Key.index(keyIn));
+			this->Key.popOut(Key.index(keyIn));
+		}
+		unsigned __int64 length() {
+			return this->Tuple.length();
+		}
+};
+
+class Object{
+	private:
+		string Type;
+		fltDic Stat;
+		float posX;
+		float posY;
+		Sprite sprite;
+		Texture texture;
+	public:
+		void addStat(string KeyIn, float val) {
+			Stat.append(KeyIn, val);
+		}
+		float getStat(string KeyIn) {
+			return Stat.get(KeyIn);
+		}
+		string type() {
+			return this->Type;
+		}
+		void Is(string type) {
+			this->Type = type;
+		}
+		string nowIs() {
+			return this->Type;
+		}
+};
+
 int main(){
-	strDic A;
-	A.append("Name", "Billy");
-	A.append("Age", "18");
-	cout << A.get("Age") << '\n';
-	A.remove("Age");
-	A.append("Gender", "Male");
-	cout << A.get("Gender") << '\n';
+	Object Billy, Pump;
+	Billy.Is("Human");
+	Billy.addStat("Health", 100);
+	Billy.addStat("Mana", 500);
+	cout << Billy.getStat("Health") << '\n';
+	Pump.Is("Building");
+	Pump.addStat("Durability", 128);
+	cout << Pump.getStat("Durability") << '\n';
+	
 	return 0;
 }
