b2d4a6ebfdc58afb9acec7059c49aa2ae97b6509
BillyBacker <38096723+BillyBacker@users.noreply.github.com>
2020-09-06 19:24:40 +0700

    Object.Is() is now only use in other class for defining Object name. And removed objPtrDic and use objPtrTuple instead. For less complexity of code. Object name will define by default using same data in Map.registerObject("Name").
    in update function in gameEngine class, Added Event checker that will check for position of every Object in the game, and sort queue for Object to draw on screen. So now game is now capable for Object depth. If Object is a bit on above of another Object. It will be detect that this Object is behide the another Object and it will be kept for drawing later. Waiting for frontline Object to be drawn first.

--- First_step/First_step.cpp
+++ First_step/First_step.cpp
@@ -8,6 +8,7 @@
 #include <vector>
 #include <algorithm>
 #include <typeinfo>
+#include<windows.h>
 
 using namespace sf;
 using namespace std;
@@ -136,9 +137,17 @@ class Object{
 		float offsetPosY = 0;
 		Sprite sprite;
 		Texture texture;
+		float Size;
+		int imgHeight;
 	public:
 		Object(unsigned __int64 ID) {
 			this->ID = ID;
+		}
+		int getImgHeight() {
+			return imgHeight;
+		}
+		void setImgHeight(int Height) {
+			this->imgHeight = Height;
 		}
 		string type() {
 			return Type;
@@ -157,6 +166,10 @@ class Object{
 		}
 		void setSpriteSize(float scale) {
 			this->sprite.setScale(scale, scale);
+			this->Size = scale;
+		}
+		float getSize() {
+			return this->Size;
 		}
 		void addStat(string KeyIn, float val) {
 			Stat.append(KeyIn, val);
@@ -234,50 +247,63 @@ public:
 			return -1;
 		}
 	}
+	void insert(unsigned __int64 pos, Object* In) {
+		this->Tuple.insert(Tuple.begin() + pos, In);
+	}
 };
 
 class Map {
 public:
-	strTuple Key;
 	objPtrTuple Tuple;
-	void registerObject(string Type) {
+	void registerObject(string Name) {
 		Object* p = new Object(this->Tuple.length());
-		this->Key.append(Type);
+		p->Is(Name);
 		this->Tuple.append(p);
 	}
 	Object* object(string keyIn) {
-		return this->Tuple.var(Key.index(keyIn));
+		for (int i = 0; i < this->Tuple.length(); i++) {
+			if (Tuple.var(i)->nowIs() == keyIn) {
+				return Tuple.var(i);
+			}
+		}
 	}
 	Object* objectAt(unsigned __int64 Index) {
 		return this->Tuple.var(Index);
 	}
-	void set(string keyIn, Object* In) {
-		this->Tuple.set(Key.index(keyIn), In);
-	}
-	void append(string keyIn, Object* In) {
-		this->Key.append(keyIn);
+	void append(Object* In) {
 		this->Tuple.append(In);
 	}
 	void remove(string keyIn) {
-		this->Tuple.popOut(Key.index(keyIn));
-		this->Key.popOut(Key.index(keyIn));
+		Object* ptr = this->Tuple.var(0);
+		for (int i = 0; i < this->Tuple.length(); i++) {
+			if ((ptr + i)->nowIs() == keyIn) {
+				this->Tuple.popOut(i);
+			}
+		}
+	}
+	void removeObjAt(unsigned __int64 Index) {
+		this->Tuple.popOut(Index);
+	}
+	void insertQuene(unsigned __int64 Index, Object* In) {
+		this->Tuple.insert(Index, In);
 	}
 	unsigned __int64 entityNumber() {
 		return this->Tuple.length();
 	}
-	string objectNameAt(int Index) {
-		return this->Key.var(Index);
-	}
-	string objectTypeAt(int Index) {
+	string objectTypeAt(unsigned __int64 Index) {
 		return this->Tuple.var(Index)->nowIs();
 	}
+	vector<Object*> getTuple() {
+		return this->Tuple.Tuple;
+	}
+
 };
 
 class gameEngine { //this a main class of this game. resposibility for render object, recieve input event, draw graphic.
 private:
 	//Add a variable down here.
 	Map Field;
-	int move_speed = 10;
+	int move_speed = 5;
 	int h = 900;
 	int w = 1600;
 	bool W = false;
@@ -301,11 +327,11 @@ private:
 	}
 	void initObject() {
 		this->Field.registerObject("Anchor");
-		this->Field.object("Anchor")->Is("Position_manager");
+		this->Field.object("Anchor")->setPosX(0);
+		this->Field.object("Anchor")->setPosY(0);
 		this->Field.object("Anchor")->setType("Dynamic");
 
 		this->Field.registerObject("BG");
-		this->Field.object("BG")->Is("BG");
 		this->Field.object("BG")->setSpriteTexture("assets\\graph.jpg");
 		this->Field.object("BG")->setSpriteSize(10);
 		this->Field.object("BG")->setPosX(this->Field.object("Anchor")->PosX());
@@ -313,9 +339,9 @@ private:
 		this->Field.object("BG")->setType("Dynamic");
 
 		this->Field.registerObject("Stone1");
-		this->Field.object("Stone1")->Is("Asset");
 		this->Field.object("Stone1")->setSpriteTexture("assets\\alien\\PNG\\alien_armor\\armor__0001_idle_2.png");
-		this->Field.object("Stone1")->setSpriteSize(0.1);
+		this->Field.object("Stone1")->setSpriteSize(0.6);
+		this->Field.object("Stone1")->setImgHeight(428);
 		this->Field.object("Stone1")->setPosX(this->Field.object("Anchor")->PosX());
 		this->Field.object("Stone1")->setPosY(this->Field.object("Anchor")->PosY());
 		this->Field.object("Stone1")->setType("Dynamic");
@@ -323,19 +349,39 @@ private:
 		this->Field.object("Stone1")->setOffsetPosY(10);
 
 		this->Field.registerObject("Stone2");
-		this->Field.object("Stone2")->Is("Asset");
-		this->Field.object("Stone2")->setSpriteTexture("assets\\alien\\PNG\\alien_armor\\armor__0000_idle_1.png");
-		this->Field.object("Stone2")->setSpriteSize(0.5);
+		this->Field.object("Stone2")->setSpriteTexture("assets\\alien\\PNG\\alien_armor\\armor__0001_idle_2.png");
+		this->Field.object("Stone2")->setSpriteSize(0.4);
+		this->Field.object("Stone2")->setImgHeight(428);
 		this->Field.object("Stone2")->setPosX(this->Field.object("Anchor")->PosX());
 		this->Field.object("Stone2")->setPosY(this->Field.object("Anchor")->PosY());
 		this->Field.object("Stone2")->setType("Dynamic");
 		this->Field.object("Stone2")->setOffsetPosX(15);
 		this->Field.object("Stone2")->setOffsetPosY(10);
 
+		this->Field.registerObject("Stone3");
+		this->Field.object("Stone3")->setSpriteTexture("assets\\alien\\PNG\\alien_armor\\armor__0001_idle_2.png");
+		this->Field.object("Stone3")->setSpriteSize(0.3);
+		this->Field.object("Stone3")->setImgHeight(428);
+		this->Field.object("Stone3")->setPosX(this->Field.object("Anchor")->PosX());
+		this->Field.object("Stone3")->setPosY(this->Field.object("Anchor")->PosY());
+		this->Field.object("Stone3")->setType("Dynamic");
+		this->Field.object("Stone3")->setOffsetPosX(20);
+		this->Field.object("Stone3")->setOffsetPosY(10);
+
+		this->Field.registerObject("Stone4");
+		this->Field.object("Stone4")->setSpriteTexture("assets\\stone.jpg");
+		this->Field.object("Stone4")->setSpriteSize(1);
+		this->Field.object("Stone4")->setImgHeight(1365);
+		this->Field.object("Stone4")->setPosX(this->Field.object("Anchor")->PosX());
+		this->Field.object("Stone4")->setPosY(this->Field.object("Anchor")->PosY());
+		this->Field.object("Stone4")->setType("Dynamic");
+		this->Field.object("Stone4")->setOffsetPosX(30);
+		this->Field.object("Stone4")->setOffsetPosY(10);
+
 		this->Field.registerObject("Elon");
-		this->Field.object("Elon")->Is("Player");
 		this->Field.object("Elon")->setSpriteTexture("assets\\alien\\PNG\\alien_armor\\armor__0001_idle_2.png");
 		this->Field.object("Elon")->setSpriteSize(0.3);
+		this->Field.object("Elon")->setImgHeight(428);
 		this->Field.object("Elon")->addStat("Health", 100);
 		this->Field.object("Elon")->addStat("Hunger", 100);
 		this->Field.object("Elon")->addStat("Air", 100);
@@ -344,7 +390,6 @@ private:
 		this->Field.object("Elon")->setType("Static");
 
 		this->Field.registerObject("Health_Bar");
-		this->Field.object("Health_Bar")->Is("Stat_Bar");
 		this->Field.object("Health_Bar")->addStat("Value", 100);
 		this->Field.object("Health_Bar")->setSpriteTexture("assets\\red.jpg");
 		this->Field.object("Health_Bar")->setSpriteSize(0.01);
@@ -353,6 +398,14 @@ private:
 		this->Field.object("Health_Bar")->getSprite().scale(100, 1);
 		this->Field.object("Health_Bar")->setType("Static");
 
+	}
+	void Swap(unsigned __int64 ID1, unsigned __int64 ID2) {
+		Object* ptr1 = this->Field.objectAt(ID1);
+		Object* ptr2 = this->Field.objectAt(ID2);
+		this->Field.removeObjAt(ID1);
+		this->Field.insertQuene(ID1, ptr2);
+		this->Field.removeObjAt(ID2);
+		this->Field.insertQuene(ID2, ptr1);
 	}
 public:
 	gameEngine() {
@@ -415,10 +468,10 @@ public:
 				D = false;
 			}
 			if (ev.type == Event::KeyPressed && ev.key.code == Keyboard::LShift) {
-				this->move_speed = 20;
+				this->move_speed = 8*1.5;
 			}
 			if (ev.type == Event::KeyReleased && ev.key.code == Keyboard::LShift) {
-				this->move_speed = 10;
+				this->move_speed = 8;
 			}
 		}
 		
@@ -448,6 +501,12 @@ public:
 				this->Field.objectAt(i)->setPosY(this->Field.object("Anchor")->PosY());
 			}
 		}
+		for (int i = 2; i < this->Field.entityNumber()-2; i++) {
+			if (this->Field.objectAt(i)->PosY() + (this->Field.objectAt(i)->getImgHeight() * this->Field.objectAt(i)->getSize()) > this->Field.objectAt(i + 1)->PosY() + (this->Field.objectAt(i + 1)->getImgHeight()* this->Field.objectAt(i + 1)->getSize())) {
+				printf("++++++++++++++++++++++++++++++++++ Swap 1 %d %d\n", i, i + 1);
+				this->Swap(i, i + 1);
+			}
+		}
 		/*
 		This part is for update game event.
 		*/
@@ -461,7 +520,12 @@ public:
 		3. draw
 		*/
 		for (int i = 0; i < Field.entityNumber(); i++) {
-			printf("%d; %f, %f\n", i, this->Field.objectAt(i)->PosX(), this->Field.objectAt(i)->PosY());
+			cout << i << '\t';
+			cout << this->Field.objectAt(i)->nowIs() << '\n';
+		}
+		cout << "---------------------------------\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" << '\n';
+		for (int i = 0; i < Field.entityNumber(); i++) {
+			//printf("%d; %f, %f\n", i, this->Field.objectAt(i)->PosX(), this->Field.objectAt(i)->PosY());
 			this->window->draw(this->Field.objectAt(i)->getSprite());
 		}
 		//printf("%f, %f\n", this->dui.getPosX(), this->dui.getPosY());
@@ -477,3 +541,4 @@ int main() { // Game loop
 		First_step.render();
 	}
 }
+
